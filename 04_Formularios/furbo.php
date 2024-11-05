e8bd6e5459426f8db979976cda7ee10a44e09b3d
Luis-GonzalesC <luiguigc11@gmail.com>
2024-11-05 14:42:24 +0100

    ejercicio formulario FURBO

---
+++ 04_Formularios/furbo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );
+    ?>
+    <title>Futbol</title>
+</head>
+<body>
+    <?php
+        /*FORMULARIO EQUIPOS DE FUTBOL DE ESPAÑA
+        - Equipo: entre 3 y 20 caracteres. Puede contener letras, espacios en blaco y puntos
+        - Liga: Opciones con select, Liga EA Sports, Liga Hypermotion, Primera RFEF (primera federacion/ divison)
+        - Números de jugadores: entre 26 y 32
+        - Fecha de fundación: entre hoy (dinámico) y 18 de diciembre de 1889 (inclusive)*/ 
+    ?>
+</body>
+</html>
